35dd7c2d4628708261470737769a6deaa86ced7f
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-03-16 14:28:47 +0530

    Refactor script.js

--- script.js
+++ script.js
@@ -1,75 +1,79 @@
 //  Library
-import jsYaml from 'https://cdn.skypack.dev/js-yaml';
+import jsYaml from 'https://cdn.skypack.dev/js-yaml'
 
 //  =======
 //  ON LOAD
 //  =======
 
-document.addEventListener("DOMContentLoaded", async () => {
-  labels = await fetch("./data.json").then((res) => res.json());
-  labels = labels.map(({ name, color, description }) => ({
-    name,
-    color: `#${color}`,
-    description
-  }));
-  updateLabelsList();
-});
+document.addEventListener('DOMContentLoaded', async () => {
+  const data = await fetch('./data.json').then((res) => res.json())
+  labels = formatLabels(data)
+  updateLabelsList()
+})
 
 //  ====
 //  FORM
 //  ====
 
 /** @type HTMLFormElement */
-const formElement = document.getElementById("form");
+const formElement = document.getElementById('form')
 /** @type HTMLInputElement */
-const userInput = document.getElementById("user");
+const userInput = document.getElementById('user')
 /** @type HTMLInputElement */
-const repoInput = document.getElementById("repo");
+const repoInput = document.getElementById('repo')
 
 async function handleSubmit(event) {
-  event.preventDefault();
-  labels = await fetch(
+  event.preventDefault()
+  const data = await fetch(
     `https://api.github.com/repos/${userInput.value}/${repoInput.value}/labels`
-  ).then((res) => res.json());
-  labels = labels.map(({ name, color, description }) => ({
-    name,
-    color: `#${color}`,
-    description
-  }));
-  updateLabelsList();
+  ).then((res) => res.json())
+  labels = formatLabels(data)
+  updateLabelsList()
 }
 
-formElement.addEventListener("submit", handleSubmit);
+formElement.addEventListener('submit', handleSubmit)
 
 //  ===========
 // LABELS STATE
 //  ===========
 
-/** @type HTMLUListElement */
-const labelNames = document.getElementById("label-names");
-const labelConfigs = document.getElementById("label-configs");
+/** @type HTMLDivElement */
+const labelNames = document.getElementById('label-names')
+/** @type HTMLDivElement */
+const labelConfigs = document.getElementById('label-configs')
 
-let labels = [];
+let labels = []
+
+function formatLabels(data) {
+  return data.map(({ name, color, description }) => ({
+    name,
+    color: `#${color}`,
+    description
+  }))
+}
 
 function updateLabelsList() {
   for (const label in labels) {
-    const labelItem = document.createElement("div");
-    const labelConfig = document.createElement('div');
+
+    const labelItem = document.createElement('div')
     labelItem.innerHTML = `
     <div class='label-name-container' style='--clr: ${labels[label].color};'>
-      <div class='label-name'>
-        ${labels[label].name}
-      </div>
+    <div class='label-name'>
+    ${labels[label].name}
+    </div>
     </div>
     `
+    labelNames.appendChild(labelItem)
+
+    const labelConfig = document.createElement('div')
     labelConfig.innerHTML = `
       <div class='label-config-container'>
         <pre>${jsYaml.dump([labels[label]])}</pre>
       </div>
     `
     labelConfig.classList.add('label-config')
-    labelNames.appendChild(labelItem)
     labelConfigs.appendChild(labelConfig)
+
   }
 }
 
@@ -77,9 +81,10 @@ function updateLabelsList() {
 // COPY
 // ====
 
-/** HTMLButtonElement */
-const copyYAMLButton = document.getElementById("copy-yaml");
-const copyJSONButton = document.getElementById("copy-json");
+/** @type HTMLButtonElement */
+const copyYAMLButton = document.getElementById('copy-yaml')
+/** @type HTMLButtonElement */
+const copyJSONButton = document.getElementById('copy-json')
 
 function onCopy(type) {
   const text = type === 'yaml'
@@ -88,5 +93,5 @@ function onCopy(type) {
   navigator.clipboard.writeText(text)
 }
 
-copyYAMLButton.addEventListener("click", () => onCopy('yaml'));
-copyJSONButton.addEventListener("click", () => onCopy('json'));
+copyYAMLButton.addEventListener('click', () => onCopy('yaml'))
+copyJSONButton.addEventListener('click', () => onCopy('json'))
